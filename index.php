df591285d36e16b6f5299591f2fc616cab89467b
Kevin Konrath <konrath.kevin@gmail.com>
2018-06-10 21:54:42 +0200

    création_index et style

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bataille Navale de la Tour K</title>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+	<meta name="description" content="Bataille Navale réalisée par la Team Tour-K" />
+	<script type="text/javascript"></script>
+</head>
+<body>
+	<h1>Bataille Navalle<h1> 
+
+
+</body>
+</html>
